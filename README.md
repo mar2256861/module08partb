3cd14e0e415bc27261cd501a832d65a66f7add73
gitmacarr <mar2256861@maricopa.edu>
2025-03-29 01:33:19 -0700

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Test
+This is a test to show I know how to put a repository on github.
---
+++ helloworld.py
@@ -0,0 +1,9 @@
+"""
+Mark Carr
+Module 08 Lab Assignment
+Part B
+
+This was to show I know how to initialize a repository
+"""
+
+print("Hello World!!")
